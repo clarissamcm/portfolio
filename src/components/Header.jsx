ad699f0c1e734b69d96d417b8a997d21cdff49eb
clarissamcm <mlearncm@gmail.com>
2025-04-16 22:17:14 -0400

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="mb-16">
+      <h1 className="text-4xl font-semibold mb-2">Matthieu Renaut</h1>
+      <p className="text-gray-600">Engineer / Creative Technologist / Paris</p>
+    </header>
+  );
+}
